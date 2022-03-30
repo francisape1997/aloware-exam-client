bed3e844a9e882274dc5470423224e9543b17f6a
Francis <90559251+francisape1997@users.noreply.github.com>
2022-03-30 22:38:10 +0800

    Update request url

--- src/services/request.js
+++ src/services/request.js
@@ -2,8 +2,10 @@ import axios from 'axios';
 
 export function request(url, method, data = [])
 {
+    const API_URL = 'http://alowaretest.local/api'
+
     return axios({
-        url: `http://alowaretest.local/api/${url}`, // Your api hostname here
+        url: `${API_URL}/${url}`, // Your api hostname here
         method,
         data,
     })
