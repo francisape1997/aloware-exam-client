a67ad7be9f7133bf2dc112c0a7abe16a28b10acd
Francis <90559251+francisape1997@users.noreply.github.com>
2022-03-30 16:52:33 +0800

    Add fragment component

---
+++ src/components/Fragment.vue
@@ -0,0 +1,17 @@
+<template>
+    <div v-frag>
+        <slot />
+    </div>
+</template>
+
+<script>
+import frag from 'vue-frag';
+
+export default {
+    name: 'Fragment',
+
+    directives: {
+        frag,
+    }
+};
+</script>
